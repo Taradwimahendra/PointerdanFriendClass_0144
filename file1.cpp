3972fece2b2e0a680b35c6b0dd9af5dd51cba899
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-06 18:35:15 +0700

    membuat class lingkaran

--- file1.cpp
+++ file1.cpp
@@ -2,3 +2,39 @@
 using namespace std;
 
 class PersegiPanjang
+{
+public :  // akses modifier 
+    int panjang, lebar;
+
+public : 
+    void input()
+    {
+        cout << "Masukkan Panjang : ";
+        cin >> panjang;
+        cout << "Masukkan Lebar : ";
+        cin >> lebar;
+    }
+
+    int LuasPp(int a, int b)
+    {
+        return a * b;
+    }
+    // lingkaran ol;
+};
+
+class Lingkaran
+{
+public : 
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan Jari-Jari : ";
+        cin >> jarijari;
+    }
+
+    int LuasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
