d14db8f3281f950edde14fa88b8e54428fab480a
Taradwimahendra15 <taradwimahendra15@gmail.com>
2025-05-06 19:11:42 +0700

    membuat class pelajar dan class manusia

--- friendfunction2.cpp
+++ friendfunction2.cpp
@@ -1,2 +1,24 @@
 #include<iostream>
 using namespace std;
+
+class pelajar;
+class manusia 
+{
+    public: 
+    void showNimPelajar(pelajar &x);
+};
+
+class pelajar 
+{
+    private:
+    int nilai;
+
+    public:
+    pelajar() { nilai =100; }
+    friend void manusia::showNimPelajar(pelajar &x);
+};
+
+void manusia::showNimPelajar(pelajar &x)
+{
+    cout << x.nilai;
+}
